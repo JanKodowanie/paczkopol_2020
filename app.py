77eb1e612ea6473ba02ae8c593314589f73e2e50
dobrowj1 <01142560@pw.edu.pl>
2020-10-12 09:22:39 +0200

    Serving html: try1

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Howdy pardner</h1>"
+    return render_template('gallery.html')
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000, debug=True)
