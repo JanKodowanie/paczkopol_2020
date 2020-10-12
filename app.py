48d4aa1fd4fac3dd11b642b1a14b55aa532f8fb9
dobrowj1 <01142560@pw.edu.pl>
2020-10-12 10:08:24 +0200

    Lab1 task done -- forms

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('gallery.html')
+    return render_template('start.html')
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000, debug=True)
