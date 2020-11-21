cad3bc2e4561606c735e6c480868032f39ecb79f
dobrowj1 <01142560@pw.edu.pl>
2020-11-21 23:26:35 +0100

    Working registration

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, make_response, request, session
+from flask import Flask, render_template, make_response, request, session, jsonify
 from flask_session import Session
 from os import getenv
 from dotenv import load_dotenv
 from redis import StrictRedis
 from bcrypt import checkpw, gensalt, hashpw
+
 
 app = Flask(__name__)
 app.debug = False
@@ -18,29 +19,42 @@ app.config.from_object(__name__)
 app.secret_key = getenv("SECRET_KEY")
 ses = Session(app)
 
+
 def save_user(validated_data):
     salt = gensalt()
-    password = password.encode()
+    password = validated_data['password'].encode()
     hashed_pw = hashpw(password, salt)
     validated_data['password'] = hashed_pw
     login = validated_data.pop('login')
     validated_data.pop('password2')
 
     try:
-        for k, v in validated_data:
+        for k, v in validated_data.items():
             db.hset(f'user: {login}', k, v)
     except Exception:
         return False
 
     return True
 
+
+def check_if_user_exists(login):
+    return db.hexists(f'user: {login}', 'email')
+
+
 @app.route('/')
 def start():
     return render_template('start.html')
 
+
 @app.route('/sender/register', methods=['GET'])
 def registration_view_get():
     return render_template('registration.html')
+
+
+@app.route('/sender/register/check-login/<login>', methods=['GET'])
+def registration_check_login(login):
+    return jsonify(available= not check_if_user_exists(login)), 200
+
 
 @app.route('/sender/register', methods=['POST'])
 def registration_view_post():
@@ -52,13 +66,16 @@ def registration_view_post():
     data['password'] = request.form.get("password", None)
     data['password2'] = request.form.get("password2", None)
     data['address'] = request.form.get("address", None)
+    
+    for k, v in data.items():
+        if not v:
+            return jsonify(error=f"{k} field cannot be empty."), 400
 
     save_user(data)
-
     response = make_response("", 301)
     response.headers["Location"] = "/sender/login"
-    return response
 
+    return response
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
